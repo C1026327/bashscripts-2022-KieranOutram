b0f01f653ee6a7729403c2143db637df508fce4f
C1026327 <c1026327@my.shu.ac.uk>
2022-11-29 15:31:04 +0000

    test

--- bash/assignment.bash
+++ bash/assignment.bash
@@ -6,15 +6,17 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
-while true; do
-    read input
-    if [ $input == 'Apache2' ]
-    then 
-      installApache2()
-    fi
-    if [ $input == 'Exit' ]
-    then 
-      echo "Goodbye"
-      break
-    fi
+
+
+while true;
+do
+read input
+if [ "$input" =='Apache2' ] 
+then installApache2() 
+fi      
+if [ "$input" == 'Exit' ] 
+then 
+echo "goodbye"
+break
+fi
 done
