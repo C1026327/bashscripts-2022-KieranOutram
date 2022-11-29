0dfac912fea6498074c4adc188f8e7dd413dd7b6
C1026327 <c1026327@my.shu.ac.uk>
2022-11-29 14:54:37 +0000

    test

--- bash/assignment.bash
+++ bash/assignment.bash
@@ -7,11 +7,12 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 while [1==1]{
-    read input
-    if [input == "Apache2"]
-        then installApache2()
-        fi
-    if [input == "Exit"]
-     then exit
-     fi
+read input
+if [ $input == 'Apache2' ]
+ then installApache2()
+fi
+if [ $input == 'Exit' ]
+ then echo "Goodbye"
+ exit
+fi
 }
