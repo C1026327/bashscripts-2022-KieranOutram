251e5d70202408c5abbb1567fd43447f6e13e6d6
C1026327 <c1026327@my.shu.ac.uk>
2022-11-29 16:57:58 +0000

    BashTest

--- bash/assignment.bash
+++ bash/assignment.bash
@@ -4,19 +4,16 @@ function installApache2(){
 }
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
-  exit
+  exit  
 fi
-
-
 while true;
 do
-read input
-if [ "$input" =='Apache2' ] 
-then installApache2() 
-fi      
-if [ "$input" == 'Exit' ] 
-then 
-echo "goodbye"
-break
-fi
+  read input
+  if [ $input == "apache2" ] || [ $input == "Apache2" ]
+    then installApache2
+  fi
+  if [ $input == 'Exit' ] || [ $input == 'exit' ]
+    then echo "Goodbye"
+    break
+  fi
 done
