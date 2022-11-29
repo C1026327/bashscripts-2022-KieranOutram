ff9e18f2a651605643610895a4bd7f03ac695702
C1026327 <c1026327@my.shu.ac.uk>
2022-11-29 14:47:21 +0000

    Foundation Set

--- bash/assignment.bash
+++ bash/assignment.bash
@@ -1,8 +1,17 @@
 #!/bin/sh
 function installApache2(){
-            
+    apt install apache2   
 }
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
+while [1==1]{
+    read input
+    if [input == "Apache2"]
+        then installApache2()
+    fi
+    if [input == "Exit"]
+     then exit
+    fi
+}
