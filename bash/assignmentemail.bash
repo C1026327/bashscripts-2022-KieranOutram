8f021c9f36077d802d1de9d6f243fc1145ca9bd8
C1026327 <c1026327@my.shu.ac.uk>
2022-12-05 15:42:38 +0000

    simplified email

--- bash/assignmentemail.bash
+++ bash/assignmentemail.bash
@@ -6,28 +6,6 @@ fi
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install mailutils -y
-echo "Welcome to the mailing service."
-echo "To Begin, submit your email by typing 'NewEmail'"
-while true;
-do 
-    read COMMAND
-    echo -n "You have selected $COMMAND"
-    echo ""
-    case ${COMMAND,,} in
-        newemail|"new email")
-            echo "Please enter your Email."
-            read EMAIL
-            echo $EMAIL > Stored_Email.txt
-            ;;
-        sendemail|"send email")
-            storedemail= cat Stored_Email.txt
-            mail -s 'AssignmentEmail.bash' "$storedemail" <<< "You have sent an email from Bash."
-            ;;
-        exit)
-            break
-            ;;
-        *)
-            echo "Unknown Command"
-            ;;
-    esac
-done
+echo "Please enter your Email."
+read EMAIL
+mail -s 'AssignmentEmail.bash' "$EMAIL"
