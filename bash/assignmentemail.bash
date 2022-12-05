e0c047594785259521e78e05a368a00c6991080a
C1026327 <c1026327@my.shu.ac.uk>
2022-12-05 18:09:45 +0000

    Security Measures blocking Emails

--- bash/assignmentemail.bash
+++ bash/assignmentemail.bash
@@ -6,6 +6,11 @@ fi
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install mailutils -y
+sudo apt-get install postfix -y
+sudo apt-get install libsas2-2 -y
+sudo apt-get install ca-certificates -y
+sudo apt-get install libsas2-modules -y
 echo "Please enter your Email."
 read EMAIL
 mail -s 'AssignmentEmail.bash' "$EMAIL"
+cat /var/mail/root
